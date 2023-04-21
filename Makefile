01465a76abcc3db5abb9a172108743236d8dd1df
Diego Daniel Dorado Ochoa <didando8a@gmail.com>
2023-04-22 00:26:11 +0100

    Add bundle class

---
+++ Makefile
@@ -0,0 +1,90 @@
+.DEFAULT_GOAL := help
+.SILENT:
+
+# Colours used in help
+GREEN    := $(shell tput -Txterm setaf 2)
+WHITE    := $(shell tput -Txterm setaf 7)
+YELLOW   := $(shell tput -Txterm setaf 3)
+RESET    := $(shell tput -Txterm sgr0)
+
+HELP_FUN = %help; \
+	while(<>) { push @{$$help{$$2 // 'Misc'}}, [$$1, $$3] \
+	if /^([a-zA-Z0-9\-]+)\s*:.*\#\#(?:@([a-zA-Z\-]+))?\s(.*)$$/ }; \
+	for (sort keys %help) { \
+	print "${WHITE}$$_${RESET}\n"; \
+	for (@{$$help{$$_}}) { \
+	$$sep = " " x (32 - length $$_->[0]); \
+	print "  ${YELLOW}$$_->[0]${RESET}$$sep${GREEN}$$_->[1]${RESET}\n"; \
+	}; \
+	print "\n"; } \
+	$$sep = " " x (32 - length "help"); \
+	print "${WHITE}Options${RESET}\n"; \
+	print "  ${YELLOW}help${RESET}$$sep${GREEN}Prints this help${RESET}\n";
+
+help:
+	@echo "\nUsage: make ${YELLOW}<target>${RESET}\n\nThe following targets are available:\n";
+	@perl -e '$(HELP_FUN)' $(MAKEFILE_LIST)
+
+################
+### Building ###
+################
+
+deps:
+#	test -f .clasp.json || cp .clasp.json.dist .clasp.json
+
+dev: deps build dependencies-install up ##@Docker Brings up the local development stack from nothing
+
+up: ##@Docker Brings up the local development stack
+	docker-compose up -d
+
+build: deps ##@Docker Builds any missing or out-of-date container images
+	docker-compose build
+
+start: ##@Docker Starts the local development stack without rebuilding container images or installing dependencies
+	docker-compose start
+
+stop: ##@Docker Stops the local development stack
+	docker-compose stop
+
+destroy: ##@Docker Tears down the entire local development stack
+	docker-compose down -v --remove-orphans
+
+logs: ##@Docker Displays and follows the Docker container logs
+	docker-compose logs -f
+
+ps: ##@Docker Gets the running status of the containers
+	docker-compose ps
+
+dev-shell: ##@Docker Drops you into a shell inside the worker service
+	docker-compose run --no-deps --rm node /bin/bash
+
+####################
+### Dependencies ###
+####################
+
+dependencies-install: ##@Dependencies Installs the NPM dependencies
+	docker-compose run --no-deps --rm node npm install
+
+#############
+### Tests ###
+#############
+
+tests-unit: ##@Tests Runs all unit tests
+	docker-compose run --rm node npm run test-unit
+
+tests-all: ##@Tests Runs all tests
+	make tests-unit
+
+
+tests-suite: ##@Test Runs all tests in a filename. Use: make tests-suite filepath=tests/functional/loanEvents/distributions/writeoff.spec.js
+	docker-compose run --rm node npm run test-suite -- $(filepath)
+
+###############
+### Quality ###
+###############
+
+lint: ##@Quality Runs linting analysis
+	docker-compose run --no-deps --rm node npm run lint
+
+lint-fix: ##@Quality Runs linting analysis automatically fixing what can be fixed
+	docker-compose run --no-deps --rm node npm run lint:fix
--- src/main.js
+++ src/main.js
@@ -1,5 +1,129 @@
-function helloNpm() {
-    return "hello NPM"
+const path = require('path');
+const fs = require('fs');
+
+class Bundler {
+    fullPathToFiles;
+    validFiles = ['.js']
+
+    constructor(pathsToFiles) {
+        if (pathsToFiles instanceof String) {
+            const pathToDir = path.join(process.cwd(), pathsToFiles);
+            const stats = fs.statSync(pathToDir);
+
+            if (!stats.isDirectory()) {
+                throw Error(`Parameter is of type string but ${pathToDir} is not a directory`);
+            }
+
+            const arrayOfFiles = this.#getFilesFromDir(pathsToFiles, []);
+
+            if (arrayOfFiles.length === 0) {
+                throw Error(`Path ${pathToDir} is a directory but do not contain any file of type ${this.validFiles.join(', ')}`);
+            }
+
+            this.fullPathToFiles = arrayOfFiles;
+        } else if (Array.isArray(pathsToFiles)) {
+            if (pathsToFiles.length === 0) {
+                throw Error(`Parameter cannot be empty`);
+            }
+
+            this.fullPathToFiles = this.#getFilesFromList(pathsToFiles);
+        }
+
+        throw Error('Parameter should be either a string referencing a directory or a set of files (at least one)');
+    }
+
+    #getFilesFromDir(pathToDir, arrayOfFiles = []) {
+        const files = fs.readdirSync(pathToDir);
+
+        files.forEach(file => {
+            const newPath =path.join(pathToDir, file);
+            if (fs.statSync(newPath).isDirectory()) {
+                arrayOfFiles = this.#getFilesFromDir(newPath, arrayOfFiles);
+            } else if (this.validFiles.includes(path.extname(newPath).toLowerCase())) {
+                arrayOfFiles.push(newPath);
+            }
+        });
+
+        return arrayOfFiles;
+    }
+
+    #getFilesFromList(pathsToFiles) {
+        return pathsToFiles.map((pathToFile) => {
+            const fullPathToFile = path.join(process.cwd(), pathToFile);
+            const stats = fs.statSync(fullPathToFile);
+
+            if (!stats.isFile()) {
+                throw Error(`${fullPathToFile} is not a not a file`);
+            }
+
+            if (!this.validFiles.includes(path.extname(fullPathToFile).toLowerCase())) {
+                throw Error(`${fullPathToFile} is not a valid file. Valid files ${this.validFiles.join(', ')}`);
+            }
+
+            return fullPathToFile;
+        });
+    }
 }
 
-module.exports = helloNpm
+function createPathFromRunningProcessFolder(pathToFile) {
+    return `${process.cwd()}/${pathToFile}`;
+}
+
+/**
+ *
+ * @param pathsList Array of file path to be bundled.
+ *  The path should be referenced from teh project root (when the process is running)
+ * @param exportNameList Functions and constants that want to be exported
+ * @param globalLibraryList libraries to be exported.
+ *  Use it to mock a library used in GAS (for instance SpreadsheetApp) by adding it
+ *  to the global object
+ * @returns {*} All files concatenated in the were added in the array
+ */
+function bundle(pathsList, exportNameList, globalLibraryList = []) {
+    if (!Array.isArray(pathsList) || pathsList.length === 0) {
+        if (!Array.isArray(pathsList)) {
+            throw Error('parameter 1 pathList must be of type array');
+        }
+
+        throw Error('parameter 1 pathList cannot be empty');
+    }
+
+    if (!Array.isArray(exportNameList) || exportNameList.length === 0) {
+        if (!Array.isArray(pathsList)) {
+            throw Error('parameter 2 exportNameList must be of type array');
+        }
+
+        throw Error('parameter 2 exportNameList cannot be empty')
+    }
+
+    let bundleFile = '';
+
+    try {
+        pathsList.forEach(pathToFile => {
+            const parsedFiled = readFileSync(
+                createPathFromRunningProcessFolder(pathToFile),
+                'utf-8',
+            );
+
+            bundleFile = ''.concat(bundleFile, '\n', parsedFiled);
+        });
+
+        globalLibraryList.forEach(globalLibName => {
+            bundleFile = ''.concat(`global.${globalLibName} = {}`, '\n', bundleFile);
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+
+    const exportTemplate = ` return {
+    ${exportNameList.join(', ')}
+};`
+
+    // eslint-disable-next-line no-new-func
+    return Function(bundleFile + exportTemplate.toString())();
+}
+
+module.exports = {
+    Bundler,
+}
